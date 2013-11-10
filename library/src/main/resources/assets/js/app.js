2f6d3eb35f15794f7b7ea34d8b3aabecfd85d653
Yang Song <ysong819@gmail.com>
2013-11-10 11:33:00 -0800

    lab 4

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,20 @@
 $(":button").click(function() {
 	var isbn = this.id;
 	alert('About to report lost on ISBN ' + isbn);
+	var bookStatusId = "#bookStatus" + isbn;
+	var buttonStatusId = "#" + isbn;
+	
+	$.ajax({
+		type: 'PUT',
+		url: '/library/v1/books/' + isbn + '?status=lost',
+		dataYype: 'application/json',
+		success: function() {	
+			$(bookStatusId).text('lost');
+			$(buttonStatusId).prop('disabled', true);
+			alert('ISBN ' + isbn + ' has been reported');
+		},
+		error: function() {
+			alert('Error!!!');
+		}
+	});
 });
